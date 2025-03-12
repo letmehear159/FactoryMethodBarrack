6fdffad678ef0939533be7a0b7dc728145bb770e
DaronD1709 <110324724+DaronD1709@users.noreply.github.com>
2025-03-12 21:41:31 +0700

    create WorkShopSquad()

--- app/src/main/java/com/example/factorymethodbarrackapplication/entity/WorkShopSquad.java
+++ app/src/main/java/com/example/factorymethodbarrackapplication/entity/WorkShopSquad.java
@@ -1,4 +1,8 @@
 package com.example.factorymethodbarrackapplication.entity;
+import java.util.HashMap;
 
-public class WorkShopSquad {
+public class WorkShopSquad extends Squad {
+    public WorkShopSquad() {
+
+    }
 }
