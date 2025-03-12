c11a1c0e3f3124b15e72d16f2c16d7eb6b7234fd
DaronD1709 <110324724+DaronD1709@users.noreply.github.com>
2025-03-12 21:39:31 +0700

    create AviarySquad()

--- app/src/main/java/com/example/factorymethodbarrackapplication/entity/AviarySquad.java
+++ app/src/main/java/com/example/factorymethodbarrackapplication/entity/AviarySquad.java
@@ -1,4 +1,8 @@
 package com.example.factorymethodbarrackapplication.entity;
+import java.util.HashMap;
 
-public class AviarySquad {
+public class AviarySquad extends Squad {
+    public AviarySquad() {
+
+    }
 }
